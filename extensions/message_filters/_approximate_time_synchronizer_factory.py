4d85f6226825a5e838e53b53f351a8bb8467b94f
Nenetti <akinori1090@gmail.com>
2022-06-30 16:51:08 +0900

    add method chain and type hint

--- extensions/message_filters/_approximate_time_synchronizer_factory.py
+++ extensions/message_filters/_approximate_time_synchronizer_factory.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Final, Type
+from typing import Any, Callable, Final, Type
 
 import genpy
 import message_filters
@@ -13,36 +13,41 @@ __all__ = [
 class ApproximateTimeSynchronizerFactory:
 
     _subscriber_kwargs: Final[dict[str, dict[Any]]]
-    _synchronizer: (message_filters.ApproximateTimeSynchronizer | None)
     _queue_size: Final[int]
     _synchronize_time_error: Final[float]
+    _callback: Final[Callable]
 
-    def __init__(self, synchronize_time_error=0.1, queue_size=10) -> None:
+    def __init__(self, callback, synchronize_time_error=0.1, queue_size=10) -> None:
         self._subscriber_kwargs = {}
-        self._synchronizer = None
         self._synchronize_time_error = synchronize_time_error
         self._queue_size = queue_size
+        self._callback = callback
 
     # ==================================================================================================
     #
     #   Public Methods
     #
     # ==================================================================================================
-    def register_subscriber(self, topic: str, data_class: Type[genpy.Message], queue_size: int = 1) -> None:
-        self._subscriber_kwargs[topic] = {"name": topic, "data_class": data_class, "queue_size": queue_size}
+    def register_subscriber(
+            self, topic: str, data_class: Type[genpy.Message], queue_size: int = 1
+    ) -> ApproximateTimeSynchronizerFactory:
+        self._subscriber_kwargs[topic] = {"data_class": data_class, "queue_size": queue_size}
+        return self
 
-    def create(self):
-        filtered_subscribers = [message_filters.Subscriber(**kwargs) for kwargs in self._subscriber_kwargs.values()]
-        self._synchronizer = ApproximateTimeSynchronizer(
-            filtered_subscribers, self._queue_size, self._synchronize_time_error
-        )
+    def create(self) -> ApproximateTimeSynchronizer:
+        filtered_subscribers = [
+            message_filters.Subscriber(topic, **kwargs) for topic, kwargs in self._subscriber_kwargs.items()
+        ]
+        synchronizer = ApproximateTimeSynchronizer(filtered_subscribers, self._queue_size, self._synchronize_time_error)
+        synchronizer.registerCallback(self._callback)
+        return synchronizer
 
 
 class ApproximateTimeSynchronizer(message_filters.ApproximateTimeSynchronizer):
 
     _subscribers: Final[list[message_filters.Subscriber]]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super(ApproximateTimeSynchronizer, self).__init__(*args, **kwargs)
         self._subscribers = args[0]
 
